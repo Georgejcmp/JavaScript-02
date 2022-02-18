a583e01538cd60b7ba3693bdd9cd8914901a9de2
George Moreno <george.moreno@yahoo.com.br>
2022-02-17 22:00:32 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var a = document.getElementById('area')
+
+function clicar() {
+    a.innerText = 'Clicou'
+    a.style.background = 'red'
+}
+function entrar() {
+    a.innerText = 'Entou!'
+}
+function sair() {
+    a.innerText = 'Saiu'
+    a.style.background = 'green'
+}
